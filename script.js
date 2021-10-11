7f46ae33cb1dad9636124bfab203f9cf3d49269e
Leonard main PC <leonard.petrosyan13@gmail.com>
2021-10-11 16:27:14 +0400

    Added countries list getting

--- script.js
+++ script.js
@@ -34,3 +34,63 @@ function showNextSlide() {
     nextSlide.classList.remove("hidden");
     active.classList.add("hidden");
 }
+
+var sameAs = document.querySelector(".slide-title-btn");
+
+sameAs.addEventListener("click", a);
+
+function a() {
+    var billingInputs = document.querySelectorAll("[data-paste]");
+
+    billingInputs.forEach(function (input) {
+        var pasteWord = input.dataset.paste;
+        var shippingInput = document.querySelector("[data-copy='" + pasteWord + "']");
+        if (shippingInput) {
+            input.value = shippingInput.value;
+        }
+    })
+
+
+}
+
+
+var loc = document.querySelector(".locationIcon");
+loc.addEventListener("click", getLocation);
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function showPosition(position) {
+    var xml = new XMLHttpRequest();
+    xml.open("get", `https://api.opencagedata.com/geocode/v1/json?q=${position.coords.latitude}+${position.coords.longitude}&key=8e318e690e8343a4b85380909e7186d5&language=en`, true);
+    xml.send();
+    xml.onload = function () {
+        if (xml.readyState === 4 && xml.status === 200) {
+            var res = JSON.parse(xml.response)
+            var city = document.querySelector("[name = shippingCity]");
+            city.value = res.results[0].components.city;
+        }
+    }
+}
+
+window.onload = function () {
+    var xml = new XMLHttpRequest();
+    xml.open("get", 'http://api.first.org/data/v1/countries', true);
+    xml.onload = function () {
+        if (xml.readyState === 4 && xml.status === 200) {
+            var sel = document.querySelectorAll("[name = shippingCountry], [name = billingCountry]")
+            var res = JSON.parse(xml.response);
+            for (var key in res.data) {
+                var country = res.data[key].country;
+                var option = document.createElement("option");
+                option.value = country;
+                option.textContent = country;
+                sel[0].append(option);
+            }
+        }
+    };
+    xml.send();
+};
